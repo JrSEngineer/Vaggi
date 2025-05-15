58d4d82d4f02d8ccc200ff2cf502221b45bb7a01
JrSEngineer <dev.jr.s.engineer@gmail.com>
2025-05-15 10:03:52 -0300

    Adding BaseEntity as base to companies and candidates.

---
+++ Vaggi/Domain/Base/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Vaggi.Domain.Base;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public string Document { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string? ProfileImage { get; set; }
+}
